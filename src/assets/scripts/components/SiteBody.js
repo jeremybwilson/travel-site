cfdcfd5d4e89a43530e76ea726a2797cd265931a
Jeremy Wilson <jeremy_b_wilson@hotmail.com>
2020-03-14 19:45:19 -0700

    Complete our Features section.

--- src/assets/scripts/components/SiteBody.js
+++ src/assets/scripts/components/SiteBody.js
@@ -76,25 +76,43 @@ class SiteBody extends React.Component {
         </div>
 
         <div id="features" className="page-section page-section--blue">
-          <div className=" wrapper">
-            <img src="./assets/images/icons/star.svg" />
-            <h2>Our Features</h2>
+          <div className="wrapper">
+            <h2 className="section-title"><img className="section-title__icon" src="./assets/images/icons/star.svg" /> Our <span className="strong">Features</span></h2>
 
-            <img src="./assets/images/icons/rain.svg" />
-            <h3>We&rsquo;ll Watch the Weather</h3>
-            <p>Download our app and we&rsquo;ll send you a notice if it&rsquo;s about to rain in the next 20 minutes around your current location. A good rain can be refreshing, but sometimes you just want to stay dry.</p>
+            <div className="row row--gutters-large generic-content-container">
+            
+              <div className="row__medium-6">
+              
+                <div className="feature-item">
+                  <img className="feature-item__icon" src="./assets/images/icons/rain.svg" />
+                  <h3 className="feature-item__title">We&rsquo;ll Watch the Weather</h3>
+                  <p className="feature-item__text">Download our app and we&rsquo;ll send you a notice if it&rsquo;s about to rain in the next 20 minutes around your current location. A good rain can be refreshing, but sometimes you just want to stay dry.</p>
+                </div>
+                
+                <div className="feature-item">
+                  <img className="feature-item__icon" src="./assets/images/icons/globe.svg" />
+                  <h3 className="feature-item__title">Global Guides</h3>
+                  <p>We&rsquo;ve scoured the entire planet for the best retreats and beautiful vistas. If there&rsquo;s a corner of the world you want to escape to we know the most scenic and inspiring locations.</p>
+                </div>
+              </div>
+              
+              <div className="row__medium-6">
+                <div className="feature-item">
+                  <img className="feature-item__icon" src="./assets/images/icons/wifi.svg" />
+                  <h3 className="feature-item__title">Wi-Fi Waypoints</h3>
+                  <p>We only send you on trips to places we can personally vouch for as being amazing. Which means we&rsquo;ve mapped out where local wi-fi spots are and marked them in our app&rsquo;s map view.</p>
+                </div>
 
-            <img src="./assets/images/icons/globe.svg" />
-            <h3>Global Guides</h3>
-            <p>We&rsquo;ve scoured the entire planet for the best retreats and beautiful vistas. If there&rsquo;s a corner of the world you want to escape to we know the most scenic and inspiring locations.</p>
+                <div className="feature-item">
+                  <img className="feature-item__icon" src="./assets/images/icons/fire.svg" />
+                  <h3 className="feature-item__title">Survival Kit</h3>
+                  <p>Everytime you book an escape with us we send you a survival kit with the finest materials. The kit will allow you to setup a tent, start a fire, scratch your own back and lower your taxes.</p>
+                </div>
+              </div>
+            </div>
 
-            <img src="./assets/images/icons/wifi.svg" />
-            <h3>Wi-Fi Waypoints</h3>
-            <p>We only send you on trips to places we can personally vouch for as being amazing. Which means we&rsquo;ve mapped out where local wi-fi spots are and marked them in our app&rsquo;s map view.</p>
 
-            <img src="./assets/images/icons/fire.svg" />
-            <h3>Survival Kit</h3>
-            <p>Everytime you book an escape with us we send you a survival kit with the finest materials. The kit will allow you to setup a tent, start a fire, scratch your own back and lower your taxes.</p>
+
           </div>
         </div>
 
