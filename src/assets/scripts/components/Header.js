374978eafef771673e6fc0037b19d53c70e4397e
Jeremy Wilson <jeremy_b_wilson@hotmail.com>
2020-03-14 18:23:26 -0700

    Added list items to Header

--- src/assets/scripts/components/Header.js
+++ src/assets/scripts/components/Header.js
@@ -2,17 +2,25 @@ import React from 'react';
 
 const Header = (props) => {
   return(
-    <header>
-      <img src="./assets/images/icons/clear-view-escapes.svg" />
-      <a href="#" className="btn">Get in Touch</a>
-      <nav>
-        <ul>
-          <li><a href="#our-beginning">Our Beginning</a></li>
-          <li><a href="#features">Features</a></li>
-          <li><a href="#testimonials">Testimonials</a></li>
-        </ul>
-      </nav>
-    </header>
+    <React.Fragment>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+      </ul>
+      <header>
+        <img src="./assets/images/icons/clear-view-escapes.svg" />
+        <a href="#" className="btn">Get in Touch</a>
+        <nav>
+          <ul>
+            <li><a href="#our-beginning">Our Beginning</a></li>
+            <li><a href="#features">Features</a></li>
+            <li><a href="#testimonials">Testimonials</a></li>
+          </ul>
+        </nav>
+      </header>
+    </React.Fragment>
   );
 }
 
