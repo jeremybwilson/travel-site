2422883cfdf6c9af50bb66638d90f85da8252f61
Jeremy Wilson <jeremy_b_wilson@hotmail.com>
2020-03-18 11:35:32 -0700

    Using JS Modules
    - added modules subfolder
    - add Person class into vendor js file

--- src/assets/scripts/vendor.js
+++ src/assets/scripts/vendor.js
@@ -1,7 +1,22 @@
-// console.log('Here is the bundle.js file.');
+import Person from './modules/Person';
 
 if(module.hot){
   module.hot.accept();
 }
+
+/* Lesson example code below this line */
+class Adult extends Person {
+  payTaxes(){
+    console.log(`${this.name} now owes zero taxes.`);
+  }
+}
+
+let john = new Person("John Doe", "blue");
+john.greet();
+
+// let jane = new Person("Jane Smith", "green");
+let jane = new Adult("Jane Smith", "green");
+// jane.greet();
+jane.payTaxes();
 
 // console.log('.:: DEBUG ::. Vendor js file is loaded here.');
